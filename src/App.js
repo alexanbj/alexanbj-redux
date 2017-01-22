ae1acdf4c9f688380fd80c6f71eaac2ecf26148b
Alexander Bjerkan <alb@knowit.no>
2017-01-22 15:33:23 +0100

    Add counter using component state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  increment() {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1,
+    }));
+  }
+
+  decrement() {
+    this.setState(prevState => ({
+      counter: prevState.counter - 1,
+    }));
+  }
+
   render() {
+    const { counter } = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to @alexanbj's redux implementation</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Clicked: {counter} times
+          {' '}
+          <button onClick={() => this.increment()}>+</button>
+          {' '}
+          <button onClick={() => this.decrement()}>-</button>
         </p>
       </div>
     );
